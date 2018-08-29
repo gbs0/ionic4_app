33d83f03ba425b725d49e59921bd12b5d0bd128d
gbs0 <gabriel@lewagon.org>
2018-08-29 15:12:09 -0300

    Declargin tech page from API request

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
-export class AboutPage {}
+export class AboutPage implements OnInit {
+  constructor (private newsService:NewsService) { }
+
+  ngOnInit(){
+  	this.newsService.getData('top-headlines?country=br&category=technology')
+  	.subscribe(data => {console.log(data)
+	;})
+  }
+
+}
+
